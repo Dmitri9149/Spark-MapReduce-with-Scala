4b09955f5c92a0758cf5f263d79c3387fd47d284
dmitri9149 <dmitri.as.name@gmail.com>
2020-09-11 18:08:38 +0300

    initial commit

---
+++ src/main/scala/Inscala.scala
@@ -0,0 +1,20 @@
+/* InScala */
+
+import org.apache.spark.sql.SparkSession
+
+object InScala {
+
+    def main(args: Array[String] {
+      
+        val logFile = " ../../trial_0/Alice.txt"
+        val spark = SparkSession.builder.appName("InScala").getOrCreate()
+        val logData = spark.read.textFile(logFile).cache()
+        val numAlice = logData.filter(line => line.contains("Alice")).count()
+        val numRabbit = logData.filter(line => line.contains("Rabbit")).count()
+        println(s"Lines with Alice: $numAlice, Lines with Rabbit: $numRabbit")
+        spark.stop()
+
+
+    }  
+
+}
