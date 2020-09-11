b914f781883ff0fa63db6afb15120f22470c42fe
dmitri9149 <dmitri.as.name@gmail.com>
2020-09-11 21:04:32 +0300

    log level is changed to less verbose

--- src/main/scala/InScala.scala
+++ src/main/scala/InScala.scala
@@ -6,11 +6,17 @@ object InScala {
 
     def main(args: Array[String]) {
       
-        val logFile = " ../../trial_0/Alice.txt"
-        val spark = SparkSession.builder.appName("InScala").getOrCreate()
+        val logFile = "Alice.txt"
+        val spark = SparkSession.builder
+          .appName("InScala")
+          .getOrCreate()
+
+        spark.sparkContext.setLogLevel("WARN")  
+
         val logData = spark.read.textFile(logFile).cache()
         val numAlice = logData.filter(line => line.contains("Alice")).count()
         val numRabbit = logData.filter(line => line.contains("Rabbit")).count()
+        println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
         println(s"Lines with Alice: $numAlice, Lines with Rabbit: $numRabbit")
         spark.stop()
 
