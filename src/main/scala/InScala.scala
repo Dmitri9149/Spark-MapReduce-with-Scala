1d155c3b0016c1218eccb7982b0147f825971d09
dmitri9149 <dmitri.as.name@gmail.com>
2020-09-12 10:38:11 +0300

    more transformation/actions are added

--- src/main/scala/InScala.scala
+++ src/main/scala/InScala.scala
@@ -1,6 +1,9 @@
 /* InScala */
 
+/* basing on https://spark.apache.org/docs/latest/quick-start.html */
+
 import org.apache.spark.sql.SparkSession
+import java.lang.Math
 
 object InScala {
 
@@ -11,15 +14,40 @@ object InScala {
           .appName("InScala")
           .getOrCreate()
 
-        spark.sparkContext.setLogLevel("WARN")  
 
+        val sqlContext = spark.sqlContext
+        import sqlContext.implicits._ //sqlContext OR spark implicits
+        //import spark.implicits._ //sqlContext OR spark implicits  
+
+        spark.sparkContext.setLogLevel("WARN")  
+        
+        val textFile = spark.read.textFile(logFile)
         val logData = spark.read.textFile(logFile).cache()
         val numAlice = logData.filter(line => line.contains("Alice")).count()
         val numRabbit = logData.filter(line => line.contains("Rabbit")).count()
+        val numLines = logData.count()
+        val firstLine = logData.first()  
+        val linesWithAlice = logData.filter(line => line.contains("Alice")).count()
+        val withMaxNumber = textFile.map(line => line.split(" ").size)reduce((a,b)=> Math.max(a,b))
+        val wordCount = textFile.flatMap(line => line.split(" ")).groupByKey(identity).count()
+        val wordCollection = wordCount.collect()
+
 
         println("*****************************************************************\n")
 
-        println(s"Lines with Alice: $numAlice, Lines with Rabbit: $numRabbit\n")
+        println(s"Number of lines: $numLines\n") 
+
+        println(s"First line:   $firstLine\n")
+
+        println(s"How many lines contains 'Alice':  $linesWithAlice\n")
+
+        println(s"Max number of words in one line is:  $withMaxNumber\n")
+      
+        println(s"Lines with 'Alice': $numAlice, Lines with 'Rabbit': $numRabbit\n")
+
+        println("The 20 'words' in the Dataset: \n")
+
+        wordCount.take(20).foreach(println)
 
         println("*****************************************************************\n")
 
